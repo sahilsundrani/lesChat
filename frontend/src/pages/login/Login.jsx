1464a02d521fd8238dabb78174f6c83d78641e35
Sahil Sundrani <sahilsundrani@Sahils-MacBook-Air.local>
2024-03-20 00:07:43 +0530

    designing basic ui done

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+const Login = () => {
+	return (
+		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+					Login To
+					<span className='text-blue-500'> LesChat</span>
+				</h1>
+
+				<form>
+					<div> 
+						<label className='label p-2'>
+							<span className='text-base label-text'>Username</span>
+						</label>
+						<input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base label-text'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='w-full input input-bordered h-10'
+						/>
+					</div>
+					<a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+						{"Don't"} have an account?
+					</a>
+
+					<div>
+						<button className='btn btn-block btn-sm mt-2'>Login</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+export default Login;
+/* my starter code for this file
+const Login = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+        <h1 className='text-3xl font-semibold text-center text-gray-300' >Login 
+          <span className='text-black' > LesChat</span>
+        </h1>
+
+        <form action="">
+          <div className='p-2'>
+            <br />
+            <label className="input input-bordered flex items-center gap-2 bg-gray-200 text-gray-950 p-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-90"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+              <input type="text" className="grow" placeholder="Username" />
+            </label>
+              <br />
+            <label className="input input-bordered flex items-center gap-2 bg-gray-200 text-gray-950 p-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-90"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+              <input type="password" className="grow" placeholder='Password' />
+            </label>
+          </div>
+
+          <a href='#' className="hover:underline text-blue inline-block mt-2 mb-2">Sign Up</a>
+
+          <div>
+            <button className='btn btn-block btn-active btn-neutral bg-gray-400 text-gray-600'>
+              <strong>Login</strong> 
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
+*/
