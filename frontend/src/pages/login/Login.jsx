5271b573a0dabc8026021e8c7647afbe2bca06c1
Sahil Sundrani <sahilsundrani@Sahils-MacBook-Air.local>
2024-03-20 23:59:58 +0530

    back-end integrated with react front-end

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
+
 const Login = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const { loading, login } = useLogin();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await login(username, password);
+	};
+
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Login To
-					<span className='text-blue-500'> LesChat</span>
+					Login to 
+					<span className='text-blue-500'>lesChat</span>
 				</h1>
 
-				<form>
-					<div> 
+				<form onSubmit={handleSubmit}>
+					<div>
 						<label className='label p-2'>
 							<span className='text-base label-text'>Username</span>
 						</label>
-						<input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+						<input
+							type='text'
+							placeholder='Enter username'
+							className='w-full input input-bordered h-10'
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
 					</div>
 
 					<div>
@@ -24,14 +43,18 @@ const Login = () => {
 							type='password'
 							placeholder='Enter Password'
 							className='w-full input input-bordered h-10'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
 						/>
 					</div>
-					<a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
 						{"Don't"} have an account?
-					</a>
+					</Link>
 
 					<div>
-						<button className='btn btn-block btn-sm mt-2'>Login</button>
+						<button className='btn btn-block btn-sm mt-2' disabled={loading}>
+							{loading ? <span className='loading loading-spinner '></span> : "Login"}
+						</button>
 					</div>
 				</form>
 			</div>
@@ -39,41 +62,3 @@ const Login = () => {
 	);
 };
 export default Login;
-/* my starter code for this file
-const Login = () => {
-  return (
-    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-        <h1 className='text-3xl font-semibold text-center text-gray-300' >Login 
-          <span className='text-black' > LesChat</span>
-        </h1>
-
-        <form action="">
-          <div className='p-2'>
-            <br />
-            <label className="input input-bordered flex items-center gap-2 bg-gray-200 text-gray-950 p-2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-90"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-              <input type="text" className="grow" placeholder="Username" />
-            </label>
-              <br />
-            <label className="input input-bordered flex items-center gap-2 bg-gray-200 text-gray-950 p-2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-90"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
-              <input type="password" className="grow" placeholder='Password' />
-            </label>
-          </div>
-
-          <a href='#' className="hover:underline text-blue inline-block mt-2 mb-2">Sign Up</a>
-
-          <div>
-            <button className='btn btn-block btn-active btn-neutral bg-gray-400 text-gray-600'>
-              <strong>Login</strong> 
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  )
-}
-
-export default Login
-*/
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,27 +1,60 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
+import { useState } from "react";
+import useSignup from "../../hooks/useSignup";
 
 const SignUp = () => {
+	const [inputs, setInputs] = useState({
+		fullName: "",
+		username: "",
+		password: "",
+		confirmPassword: "",
+		gender: "",
+	});
+
+	const { loading, signup } = useSignup();
+
+	const handleCheckboxChange = (gender) => {
+		setInputs({ ...inputs, gender });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await signup(inputs);
+	};
+
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Sign Up To<span className='text-blue-500'> LesChat</span>
+					Sign Up To<span className='text-blue-500'> lesChat </span>
 				</h1>
 
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div>
 						<label className='label p-2'>
 							<span className='text-base label-text'>Full Name</span>
 						</label>
-						<input type='text' placeholder='John Doe' className='w-full input input-bordered  h-10' />
+						<input
+							type='text'
+							placeholder='John Doe'
+							className='w-full input input-bordered  h-10'
+							value={inputs.fullName}
+							onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+						/>
 					</div>
 
 					<div>
 						<label className='label p-2 '>
 							<span className='text-base label-text'>Username</span>
 						</label>
-						<input type='text' placeholder='johndoe' className='w-full input input-bordered h-10' />
+						<input
+							type='text'
+							placeholder='johndoe'
+							className='w-full input input-bordered h-10'
+							value={inputs.username}
+							onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+						/>
 					</div>
 
 					<div>
@@ -32,6 +65,8 @@ const SignUp = () => {
 							type='password'
 							placeholder='Enter Password'
 							className='w-full input input-bordered h-10'
+							value={inputs.password}
+							onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
 						/>
 					</div>
 
@@ -43,17 +78,25 @@ const SignUp = () => {
 							type='password'
 							placeholder='Confirm Password'
 							className='w-full input input-bordered h-10'
+							value={inputs.confirmPassword}
+							onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })}
 						/>
 					</div>
 
-					<GenderCheckbox />
+					<GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
 
-					<a className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' href='#'>
+					<Link
+						to={"/login"}
+						className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'
+						href='#'
+					>
 						Already have an account?
-					</a>
+					</Link>
 
 					<div>
-						<button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700' disabled={loading}>
+							{loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+						</button>
 					</div>
 				</form>
 			</div>
@@ -61,42 +104,3 @@ const SignUp = () => {
 	);
 };
 export default SignUp;
-
-/* my starter code for this file
-const SignUp = () => {
-  return (
-    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-        <h1 className='text-3xl font-semibold text-center text-gray-300' >Sign Up to  
-          <span className='text-black' > LesChat</span>
-        </h1>
-
-        <form action="">
-          <div className='p-2'>
-            <br />
-            <label className="input input-bordered flex items-center gap-2 bg-gray-200 text-gray-950 p-2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-90"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-              <input type="text" className="grow" placeholder="Username" />
-            </label>
-              <br />
-            <label className="input input-bordered flex items-center gap-2 bg-gray-200 text-gray-950 p-2">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-90"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
-              <input type="password" className="grow" placeholder='Password' />
-            </label>
-          </div>
-
-          <a href='#' className="hover:underline text-blue inline-block mt-2 mb-2">Sign In</a>
-
-          <div>
-            <button className='btn btn-block btn-active btn-neutral bg-gray-400 text-gray-600'>
-              <strong>Sign Up</strong> 
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  )
-}
-
-export default SignUp
-*/
